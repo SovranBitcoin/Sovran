a0d8ca8b2642ab75dd1a6cebed7935321f67640f
Kelbie <kevin@kelbie.me>
2025-05-14 21:27:55 +0100

    add profile so users can sweep via mnemonic

--- app/settings/profile.tsx
+++ app/settings/profile.tsx
@@ -18,35 +18,66 @@ import { useNostr } from 'helper/redux/nostr';
 import Container from 'components/layout/Container';
 import Icon from 'assets/icons';
 import { showMessage } from 'helper/popup/popups';
+import { HDKey } from '@scure/bip32';
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
+
+const DERIVATION_PATH = `m/44'/129372'`;
 
 const Profile = () => {
   const theme = useSelector(memoizedGetTheme);
   const styles = createStyles(theme);
   const { currentProfile } = useNostr();
-  const [isNsecVisible, setIsNsecVisible] = useState(false);
+  const [visibleFields, setVisibleFields] = useState({
+    mnemonic: false,
+    nsec: false,
+    cashuMnemonic: false,
+  });
   const [isEditMode, setIsEditMode] = useState(false);
   const [displayName, setDisplayName] = useState(currentProfile?.name || '');
+  const [cashuMnemonic, setCashuMnemonic] = useState('');
   const navigation = useNavigation();
 
   useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={() => setIsEditMode(!isEditMode)} style={styles.headerButton}>
-          <Icon
-            name={isEditMode ? 'fluent:save-24-filled' : 'mage:edit-pen-fill'}
-            size={24}
-            color={greys(theme)[0]}
-          />
-        </TouchableOpacity>
-      ),
-    });
-  }, [navigation, isEditMode, theme]);
+    if (currentProfile) {
+      try {
+        // Calculate cashu mnemonic
+        const root = getRoot(currentProfile);
+        const path = `${DERIVATION_PATH}/0'/${currentProfile?.id}'/0/0`;
+        const seed = root.derive(path);
+        const derivedCashuMnemonic = bip39.entropyToMnemonic(seed.privateKey, wordlist);
+        setCashuMnemonic(derivedCashuMnemonic);
+      } catch (error) {
+        console.error('Error calculating cashu mnemonic:', error);
+        setCashuMnemonic('Error calculating cashu mnemonic');
+      }
+    }
+  }, [currentProfile]);
+
+  function getRoot(profile) {
+    if (profile?.root?.xpriv) {
+      const root = HDKey.fromExtendedKey(profile.root.xpriv);
+      if (root) {
+        return root;
+      }
+    }
+
+    const root = HDKey.fromMasterSeed(bip39.mnemonicToSeedSync(profile?.mnemonic));
+    return root;
+  }
 
   const handleCopy = (text, messageKey) => {
     if (text) {
       Clipboard.setString(text);
       showMessage(messageKey);
     }
+  };
+
+  const toggleFieldVisibility = (field) => {
+    setVisibleFields((prev) => ({
+      ...prev,
+      [field]: !prev[field],
+    }));
   };
 
   const renderDetail = (label, value, editable = false) => (
@@ -60,34 +91,42 @@ const Profile = () => {
     </View>
   );
 
-  const renderCopyableDetail = (label, value, messageKey, showEyeIcon = false) => (
-    <View style={styles.detailContainer}>
-      <Text style={styles.detailLabel}>{label}</Text>
-      <View style={styles.sensitiveField}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <Text style={styles.detailText}>
-            {showEyeIcon && !isNsecVisible ? '••••••••' : value || 'N/A'}
-          </Text>
-        </ScrollView>
-        <View style={styles.iconContainer}>
-          {showEyeIcon && (
+  const renderCopyableDetail = (label, value, messageKey, fieldKey = null, description = null) => {
+    const showEyeIcon = fieldKey !== null;
+    const isVisible = fieldKey ? visibleFields[fieldKey] : true;
+
+    return (
+      <View style={styles.detailContainer}>
+        <Text style={styles.detailLabel}>{label}</Text>
+        <View style={styles.sensitiveField}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <Text style={styles.detailText}>
+              {showEyeIcon && !isVisible ? '••••••••' : value || 'N/A'}
+            </Text>
+          </ScrollView>
+          <View style={styles.iconContainer}>
+            {showEyeIcon && (
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => toggleFieldVisibility(fieldKey)}>
+                <Icon
+                  name={isVisible ? 'majesticons:eye-off' : 'majesticons:eye'}
+                  size={16}
+                  color={greys(theme)[700]}
+                />
+              </TouchableOpacity>
+            )}
             <TouchableOpacity
               style={styles.iconButton}
-              onPress={() => setIsNsecVisible(!isNsecVisible)}>
-              <Icon
-                name={isNsecVisible ? 'majesticons:eye-off' : 'majesticons:eye'}
-                size={16}
-                color={greys(theme)[700]}
-              />
+              onPress={() => handleCopy(value, messageKey)}>
+              <Icon name="lets-icons:copy" size={16} color={greys(theme)[700]} />
             </TouchableOpacity>
-          )}
-          <TouchableOpacity style={styles.iconButton} onPress={() => handleCopy(value, messageKey)}>
-            <Icon name="lucide:copy" size={16} color={greys(theme)[700]} />
-          </TouchableOpacity>
+          </View>
         </View>
+        {description && <Text style={styles.descriptionText}>{description}</Text>}
       </View>
-    </View>
-  );
+    );
+  };
 
   return (
     <Container>
@@ -103,11 +142,37 @@ const Profile = () => {
           />
         </View>
 
-        {renderDetail('Username:', currentProfile?.nip05)}
-        {isEditMode && <Text style={styles.editModeText}>Edit</Text>}
-        {renderDetail('Display Name:', displayName, true)}
-        {renderCopyableDetail('npub:', currentProfile?.npub, 'npub_copied')}
-        {renderCopyableDetail('nsec:', currentProfile?.nsec, 'nsec_copied', true)}
+        {renderCopyableDetail(
+          'NIP06:',
+          currentProfile?.mnemonic,
+          'mnemonic_copied',
+          'mnemonic',
+          'Your recovery phrase that gives access to all your nostr & cashu wallets. Everything is derived from this mnemonic so keep it safe and secure!'
+        )}
+
+        {renderCopyableDetail(
+          'Npub:',
+          currentProfile?.npub,
+          'npub_copied',
+          null,
+          'Your public identifier on the Nostr network.'
+        )}
+
+        {renderCopyableDetail(
+          'Nsec:',
+          currentProfile?.nsec,
+          'nsec_copied',
+          'nsec',
+          'Your private key. Never share this with anyone.'
+        )}
+
+        {renderCopyableDetail(
+          `NUT13:`,
+          cashuMnemonic,
+          'cashu_mnemonic_copied',
+          'cashuMnemonic',
+          'This is a mnemonic you can use in other cashu wallets to recover your funds if you ever want to stop using Sovran.'
+        )}
       </SafeAreaView>
     </Container>
   );
@@ -151,9 +216,15 @@ const createStyles = (theme) =>
       fontSize: 16,
       color: greys(theme)[0],
     },
+    descriptionText: {
+      fontSize: 12,
+      fontStyle: 'italic',
+      color: greys(theme)[400],
+      marginTop: 4,
+    },
     editModeText: {
       fontSize: 12,
-      color: greys(theme)[500],
+      color: greys(theme)[400],
       marginBottom: 4,
     },
     sensitiveField: {
