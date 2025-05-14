bcf61fc1b2435bcef4330016d08c39d6f185e1b2
Kelbie <kevin@kelbie.me>
2025-05-14 13:13:18 +0100

    fix not showing old sats on inactive keysets

--- helper/cashu/keys.ts
+++ helper/cashu/keys.ts
@@ -1,17 +1,18 @@
-import { MintKeys } from '@cashu/cashu-ts';
+import { MintKeys, MintKeyset } from '@cashu/cashu-ts';
 import { setKeysets } from 'helper/redux/cashu';
 import { store } from 'helper/redux/store';
 import { getMint } from './mint';
+import { Alert } from 'react-native';
 
 interface GetKeysParams {
   unit: string;
   mintUrl: string;
 }
 
-export async function getKeys({ unit, mintUrl }: GetKeysParams): Promise<MintKeys | undefined> {
+export async function getKeys({ unit, mintUrl }: GetKeysParams): Promise<MintKeyset | undefined> {
   const mint = getMint({ mintUrl });
 
-  const keysets = (await (await mint).getKeys()).keysets;
+  const keysets = (await (await mint).getKeySets()).keysets;
 
   const key = keysets.find((k) => k.unit === unit);
 
