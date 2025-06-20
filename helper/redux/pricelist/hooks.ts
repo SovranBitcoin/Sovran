92e9402ce34ab2b4d0becb081596bd92cd983b28
Kelbie <kevin@kelbie.me>
2025-06-20 16:29:38 +0100

    fix(pricelist): stabilize websocket effect

--- helper/redux/pricelist/hooks.ts
+++ helper/redux/pricelist/hooks.ts
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
 import { setPricelist } from './actions';
 import { memoizedPricelist } from './selectors';
 
@@ -6,8 +7,13 @@ export const usePricelist = () => {
   const dispatch = useDispatch();
   const pricelist = useSelector(memoizedPricelist);
 
+  const setPricelistCallback = useCallback(
+    (pricelistData) => dispatch(setPricelist(pricelistData)),
+    [dispatch]
+  );
+
   return {
     pricelist,
-    setPricelist: (pricelistData) => dispatch(setPricelist(pricelistData)),
+    setPricelist: setPricelistCallback,
   };
 };
