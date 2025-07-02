d6073373e4c686acc3271af4c38d95ee75e4dd10
Kelbie <kevin@kelbie.me>
2025-07-02 16:22:14 +0100

    Refine mint balance sheet UI

--- components/layout/sheets/mint-balance/routes/list.tsx
+++ components/layout/sheets/mint-balance/routes/list.tsx
@@ -15,7 +15,7 @@ import { CurrencyIcon, FlagIcon } from 'assets/icons';
 import Wrapper from '../../wrapper';
 import { ButtonHandler } from 'components/common/ButtonHandler';
 import _ from 'lodash';
-import { LinearGradient } from 'expo-linear-gradient';
+import { GradientBorderView } from '@good-react-native/gradient-border';
 import opacity from 'hex-color-opacity';
 import Image from 'components/common/Image';
 import { AmountFormatter } from 'components/common/AmountFormatter';
@@ -43,24 +43,22 @@ const MintItem: React.FC<MintItemProps> = ({
 }) => {
   const styles = createStyles(theme);
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[styles.mintItem, balance.amount === 0 && styles.zeroBalance]}
-      disabled={globalLoading}
-    >
-      {mint.iconUrl ? (
-        <Image source={{ uri: mint.iconUrl }} style={styles.mintIcon} />
-      ) : (
-        <Image style={styles.mintIcon} />
-      )}
-      <View style={styles.mintDetails}>
-        <Text style={styles.mintName}>{mint.name}</Text>
-        <Text style={styles.mintBalance}>
-          <AmountFormatter size={14} amount={balance.amount} unit={balance.unit} />
-        </Text>
-      </View>
-      <View style={{ width: 16 }}>
-        {isLoading && <ActivityIndicator size="small" color={greys(theme)[0]} />}
+    <TouchableOpacity onPress={onPress} disabled={globalLoading}>
+      <View blur style={[styles.mintItem, balance.amount === 0 && styles.zeroBalance]}>
+        {mint.iconUrl ? (
+          <Image source={{ uri: mint.iconUrl }} style={styles.mintIcon} />
+        ) : (
+          <Image style={styles.mintIcon} />
+        )}
+        <View style={styles.mintDetails}>
+          <Text style={styles.mintName}>{mint.name}</Text>
+          <Text style={styles.mintBalance}>
+            <AmountFormatter size={14} amount={balance.amount} unit={balance.unit} />
+          </Text>
+        </View>
+        <View style={{ width: 16 }}>
+          {isLoading && <ActivityIndicator size="small" color={greys(theme)[0]} />}
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -151,35 +149,42 @@ const ListRoute = () => {
         </Text>
         <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.currencyScroll}>
           {currencies.map((currency) => (
-            <LinearGradient
-              key={currency}
-              colors={
-                selectedCurrency === currency
-                  ? [
-                      opacity(greys(theme)[200], 0.88),
-                      opacity(greys(theme)[200], 0.88),
-                      opacity(greys(theme)[300], 0.88),
-                      opacity(greys(theme)[200], 0.88),
-                      opacity(greys(theme)[300], 0.88),
-                    ]
-                  : [opacity(greys(theme)[200], 0), opacity(greys(theme)[200], 0)]
-              }
-              style={[styles.currencyButton, { marginRight: 8, borderRadius: 8, padding: 1, backgroundColor: greys(theme)[900] }]}
-            >
-              <TouchableOpacity
-                style={[styles.currencyButton, selectedCurrency === currency && styles.selectedCurrencyButton]}
-                onPress={() => setSelectedCurrency(currency)}
+            <TouchableOpacity key={currency} onPress={() => setSelectedCurrency(currency)}>
+              <GradientBorderView
+                gradientProps={{
+                  colors:
+                    selectedCurrency === currency
+                      ? [
+                          opacity(greys(theme)[200], 0.88),
+                          opacity(greys(theme)[300], 0.88),
+                          opacity(greys(theme)[200], 0.88),
+                          opacity(greys(theme)[300], 0.88),
+                        ]
+                      : [opacity(greys(theme)[200], 0), opacity(greys(theme)[200], 0)],
+                }}
+                style={{ borderWidth: selectedCurrency === currency ? 1 : 0, borderRadius: 8, marginRight: 8 }}
               >
-                <View style={styles.currencyContent}>
+                <View
+                  blur
+                  style={[
+                    styles.currencyContent,
+                    styles.currencyButton,
+                    selectedCurrency === currency && styles.selectedCurrencyButton,
+                  ]}
+                >
                   {currency === 'USD' || currency === 'EUR' || currency === 'GBP' ? (
-                    <FlagIcon country={currency === 'USD' ? 'US' : currency === 'EUR' ? 'EU' : 'GB'} height={32} width={32} />
+                    <FlagIcon
+                      country={currency === 'USD' ? 'US' : currency === 'EUR' ? 'EU' : 'GB'}
+                      height={32}
+                      width={32}
+                    />
                   ) : (
                     <CurrencyIcon currency={currency.toLowerCase()} />
                   )}
                   <Text style={styles.currencyText}>{displayCurrency(currency)}</Text>
                 </View>
-              </TouchableOpacity>
-            </LinearGradient>
+              </GradientBorderView>
+            </TouchableOpacity>
           ))}
         </ScrollView>
 
