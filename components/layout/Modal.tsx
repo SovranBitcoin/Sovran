5c302f11226692dcaf2b786a8e30d5fceef27838
Kelbie <kevin@kelbie.me>
2025-07-03 10:28:06 +0100

    Wrap Modal content with SheetProvider

--- components/layout/Modal.tsx
+++ components/layout/Modal.tsx
@@ -22,6 +22,7 @@ import { useNavigation } from 'expo-router';
 import opacity from 'hex-color-opacity';
 import { memoizedGetTheme } from 'helper/redux/settings';
 
+import { SheetProvider } from 'react-native-actions-sheet';
 import { registerAllSheets } from 'components/layout/sheets/registerSheets';
 
 // Register all action sheets within the modal context so that
@@ -157,16 +158,18 @@ export default function Modal({
     React.isValidElement(children) ? children : <View>{children}</View>;
 
   return (
-    <View
-      style={[
-        {
-          position: 'relative',
-          display: 'flex',
-          flex: 1,
-          backgroundColor: bgColor,
-        },
-        childrenStyles,
-      ]}>
+    <SheetProvider context="modal">
+      <View
+        style={[
+          {
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            backgroundColor: bgColor,
+          },
+          childrenStyles,
+        ]}
+      >
       {renderHeader()}
 
       <FlatList
@@ -204,5 +207,6 @@ export default function Modal({
         {buttons}
       </View>
     </View>
+    </SheetProvider>
   );
 }
