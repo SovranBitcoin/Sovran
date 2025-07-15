db6e1295b19659577a7538dba274567e0cb83ee5
Kelbie <kevin@kelbie.me>
2025-07-16 00:49:50 +0100

    Update eas-build-on-success.sh

--- eas-build-on-success.sh
+++ eas-build-on-success.sh
@@ -5,17 +5,24 @@ set -eox pipefail
 curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
+# install dcd
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 22
+npm install -g @devicecloud.dev/dcd
+
 if [ "$EAS_BUILD_PLATFORM" = "ios" ]
 then
-   brew install java
-   echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-   sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-   export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+  brew install java
+  echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+  sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+  export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 
-   APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/Sovran.app
-   
-   MAESTRO_API_KEY=$MAESTRO_API_KEY
-   dcd cloud --project-id="proj_01jvhrahrjf5dtcy4zbpewj7p2" --apiKey  $MAESTRO_API_KEY $APP_EXECUTABLE_PATH .maestro  
+  APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/Sovran.app
+
+  MAESTRO_API_KEY=$MAESTRO_API_KEY
+  dcd cloud --apiKey $MAESTRO_API_KEY $APP_EXECUTABLE_PATH .maestro  
 else
-   APP_EXECUTABLE_PATH=/home/expo/workingdir/build/android/app/build/outputs/apk/release/app-release.apk
+  APP_EXECUTABLE_PATH=/home/expo/workingdir/build/android/app/build/outputs/apk/release/app-release.apk
 fi
