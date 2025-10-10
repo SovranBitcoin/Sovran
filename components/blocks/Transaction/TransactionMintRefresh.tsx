b90c4c48c65c42e8c6d7a81859dd1a1d308f6eb2
Kelbie <kevin@kelbie.me>
2025-10-10 16:47:57 +0100

    Update TransactionMintRefresh.tsx

--- components/blocks/Transaction/TransactionMintRefresh.tsx
+++ components/blocks/Transaction/TransactionMintRefresh.tsx
@@ -7,7 +7,7 @@ import { GetInfoResponse } from '@cashu/cashu-ts';
 
 interface HistoryEntryMintRefreshProps {
   mintInfo: GetInfoResponse;
-  historyEntry: HistoryEntry;
+  historyEntry: Partial<HistoryEntry> & { type: HistoryEntry['type'] };
   handleCheckStatus?: (onClose: () => void) => Promise<void>;
 }
 
@@ -16,7 +16,7 @@ export function HistoryEntryMintRefresh({ mintInfo, historyEntry }: HistoryEntry
     <HStack
       align="center"
       justify="space-between"
-      className="bg-primary-800 rounded-lg"
+      className="rounded-lg bg-primary-800"
       style={{
         marginHorizontal: 16,
         marginBottom: 0,
