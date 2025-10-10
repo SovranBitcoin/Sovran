0dd06d5c2d6436edde2a470371bb9db21f317317
Kelbie <kevin@kelbie.me>
2025-10-10 19:47:14 +0100

    Update TimeLine.tsx

--- app/message/TimeLine.tsx
+++ app/message/TimeLine.tsx
@@ -1,4 +1,4 @@
-import { TextMessage, PaymentMessage, CashuTokenMessage } from './components';
+import { TextMessage, CashuTokenMessage } from './components';
 import { useNostr } from 'redux/nostr';
 import { isValidEcashToken } from '@/helper/coco/utils';
 import { TimelineItemType } from 'app/userMessages';
@@ -10,19 +10,12 @@ interface TimelineItemProps {
 const TimelineItem = ({ item }: TimelineItemProps) => {
   const { currentProfile } = useNostr();
 
-  // Determine item type
   const isMessage = 'content' in item && !!item.content;
-  const isTransaction = 'unit' in item && !!item.unit;
 
   const isTokenMessage = isMessage && isValidEcashToken(item.content);
 
-  // Determine if message is received
   const isMessageReceived =
-    isMessage && (item.receiver === currentProfile?.pubkey || item.id === -1);
-  const isTransactionReceived =
-    isTransaction &&
-    (('nostr' in item && item.nostr?.pubkey === currentProfile?.pubkey) ||
-      (item.receiver && item.receiver === currentProfile?.pubkey));
+    isMessage && (item.receiver === currentProfile?.pubkey || item.id === '-1');
 
   if (isTokenMessage) {
     return <CashuTokenMessage token={item.content} isReceived={isMessageReceived} />;
@@ -30,10 +23,6 @@ const TimelineItem = ({ item }: TimelineItemProps) => {
 
   if (isMessage) {
     return <TextMessage message={item as any} isReceived={isMessageReceived} />;
-  }
-
-  if (isTransaction) {
-    return <PaymentMessage transaction={item as any} isReceived={isTransactionReceived} />;
   }
 
   return null;
