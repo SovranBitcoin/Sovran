eb7cc3abd87a0e754b22291520411aebc0ddbc68
Kelbie <kevin@kelbie.me>
2026-05-15 22:02:29 +0100

    Allow @sovranbitcoin in Jest transforms
    Update jest.config.js transformIgnorePatterns to include the @sovranbitcoin scope. This ensures packages under @sovranbitcoin are transformed by Jest (not ignored), preventing test failures caused by untranspiled code in that dependency.

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,6 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/coco/', '/eNuts/', '/coco-payment-ux/'],
   modulePathIgnorePatterns: ['/coco-payment-ux/'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|nostr-tools|@scure|@noble|coco-cashu-core|@cashu/cashu-ts)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|nostr-tools|@scure|@noble|coco-cashu-core|@cashu/cashu-ts|@sovranbitcoin/.*)',
   ],
 };
